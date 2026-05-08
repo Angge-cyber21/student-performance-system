a544116ca70e30eac0480705969e973b71e1cb51
Angge-cyber21 <23-05473@g.batstate-u.edu.ph>
2026-05-08 11:22:07 +0800

    Create student_performance database and students table

---
+++ database/postgres_setup.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE student_performance;
+
+CREATE TABLE students (
+    age INT,
+    sex TEXT,
+    school TEXT,
+    address_type TEXT,
+    absences INT,
+    class_failures INT,
+    study_time TEXT,
+    performance_level TEXT
+);
